89d0e43fcaecc2c9202f232c12d4e29c4424d1f3
sahithyaagopinathan <sahithyagopinathan@gmail.com>
2026-09-22 14:57:41 +0530

    Time: 1 ms (84.95%), Space: 43.1 MB (6.7%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+    private Queue<Integer> queue;
+
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        queue.add(x);
+        // Rotate the queue so that the newly added element is at the front
+        int size = queue.size();
+        for (int i = 0; i < size - 1; i++) {
+            queue.add(queue.poll());
+        }
+    }
+    
+    public int pop() {
+        return queue.poll();
+    }
+    
+    public int top() {
+        return queue.peek();
+    }
+    
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
